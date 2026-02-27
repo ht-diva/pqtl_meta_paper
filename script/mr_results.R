7aadb7f25d55b31aa4f894080d5bce27709d8563
giuliapontali <45068286+giuliapontali@users.noreply.github.com>
2026-02-27 15:54:45 +0100

    Update mr_results.R

--- script/mr_results.R
+++ script/mr_results.R
@@ -92,3 +92,352 @@ collapsed_final<- final[, lapply(.SD, function(x) paste(unique(x), collapse = "|
                                     by = .(seqID, locus_START_END_37, BETA, PROTEIN_NAME, PHENOTYPE, CHR, PVAL, SE)]
 
 write.table(collapsed_final, "/scratch/giulia.pontali/meta_file/collapsed_final.txt", sep="\t", quote=F, row.names = F)
+
+#---
+combined_data_epitope_coloc <- combined_data_epitope_coloc[which(combined_data_epitope_coloc$sign==TRUE),]
+
+combined_data_epitope_coloc <- combined_data_epitope_coloc %>%
+  mutate(
+    HAS_INT = str_detect(PHENOTYPE, "(?i)_INT$"),
+    CORE = str_remove(PHENOTYPE, "(?i)_INT$"),
+    
+    CORE = CORE %>%
+      str_replace("(?i)^mean_([^_]+)$",   "\\1_Mean") %>%
+      str_replace("(?i)^min_([^_]+)$",    "\\1_Min") %>%
+      str_replace("(?i)^max_([^_]+)$",    "\\1_Max") %>%
+      str_replace("(?i)^median_([^_]+)$", "\\1_Median") %>%
+      
+      str_replace("(?i)_mean$",   "_Mean") %>%
+      str_replace("(?i)_min$",    "_Min") %>%
+      str_replace("(?i)_max$",    "_Max") %>%
+      str_replace("(?i)_median$", "_Median"),
+    
+    PHENOTYPE = if_else(HAS_INT, paste0(CORE, "_INT"), CORE),
+    
+    PHENOTYPE = str_replace_all(PHENOTYPE, "__", "_")
+  ) %>%
+  select(-HAS_INT, -CORE)
+
+
+
+efo <- read_excel("/scratch/giulia.pontali/pheno_updates.xlsx")
+efo <- efo[,c(1,2,17:20)]
+
+combined_data_epitope_coloc <- left_join(
+  combined_data_epitope_coloc,
+  efo %>% select(Phenotype, MVP_description, EFO_ID, EFO_Term, EFO_Parent_ID, EFO_Parent_Term),
+  by = c("PHENOTYPE" = "Phenotype")
+)
+
+
+# 1) Load + normalize drug file
+drug_indication <- read.delim("/exchange/healthds/MVP/061624_Drug_Indication_clean_hv1.2_11JUL2024.tsv")
+
+
+drug_indication <- drug_indication %>%
+  mutate(across(c(Genetic_phenotype_phecode, EFO_ID, EFO_Term, EFO_Parent_Term, EFO_Parent_ID, Matched_genetic_phenotype), 
+                ~str_replace_na(.x, ""))) %>%
+  separate_rows(Genetic_phenotype_phecode, sep="\\|", convert=FALSE) %>%
+  separate_rows(EFO_ID, sep="\\|", convert=FALSE) %>%
+  separate_rows(EFO_Term, sep="\\|", convert=FALSE) %>%
+  #separate_rows(EFO_Parent_Term, sep="\\|", convert=FALSE) %>%
+  separate_rows(EFO_Parent_ID, sep="\\|", convert=FALSE) %>%
+  separate_rows(Matched_genetic_phenotype, sep="\\|", convert=FALSE) %>%
+  mutate(across(c(Genetic_phenotype_phecode, EFO_ID, EFO_Term, EFO_Parent_Term, EFO_Parent_ID, Matched_genetic_phenotype), str_trim))
+
+
+drug_summary <- drug_indication %>%
+  select(
+    HARMONIZED_GENE_NAME, drug_indication, drug_name, moa,
+    drug_max_phase, max_phase_for_ind, year_of_first_approval,
+    parent_drug_name, Indication_disease_name,
+    Genetic_phenotype_phecode, EFO_ID, EFO_Term, EFO_Parent_ID, EFO_Parent_Term,
+    Matched_genetic_phenotype, action_type
+  ) %>%
+  distinct() %>%
+  filter(max_phase_for_ind >= 3) %>%
+  separate_rows(EFO_Parent_Term, sep = "\\|") %>%
+  mutate(
+    EFO_ID_lower     = tolower(EFO_ID),
+    EFO_Term_lower   = tolower(EFO_Term),
+    EFO_Parent_Term_lower = tolower(EFO_Parent_Term),
+    EFO_Parent_ID_lower = tolower(EFO_Parent_ID),
+    Matched_genetic_phenotype_lower = tolower(Matched_genetic_phenotype),
+    Genetic_phenotype_phecode_lower = tolower(Genetic_phenotype_phecode)
+  ) %>%
+  select(
+    -EFO_ID, -EFO_Term, -EFO_Parent_Term, -EFO_Parent_ID,
+    -Matched_genetic_phenotype, -Genetic_phenotype_phecode
+  )
+
+gene_drug_info <- drug_summary %>%
+  group_by(HARMONIZED_GENE_NAME) %>%
+  summarise(
+    all_drug_names = paste(unique(drug_name), collapse = " | "),
+    all_indications = paste(unique(Indication_disease_name), collapse = " | "),
+    .groups = "drop"
+  )
+
+combined_data_epitope_coloc <- combined_data_epitope_coloc %>%
+  mutate(row_id = row_number()) %>%
+  mutate(across(c(HARMONIZED_GENE_NAME, PHENOTYPE, EFO_Term, EFO_ID, EFO_Parent_Term, EFO_Parent_ID),
+                ~ str_replace_na(.x, ""))) %>%
+  mutate(across(c(HARMONIZED_GENE_NAME, PHENOTYPE, EFO_Term, EFO_ID, EFO_Parent_Term, EFO_Parent_ID),
+                str_trim)) %>%
+  separate_rows(EFO_ID, sep = "\\|") %>%
+  separate_rows(EFO_Term, sep = "\\|") %>%
+  separate_rows(EFO_Parent_Term, sep = "\\|") %>%
+  separate_rows(EFO_Parent_ID, sep = "\\|") %>%
+  mutate(
+    EFO_ID_lower     = str_trim(tolower(EFO_ID)),
+    EFO_Term_lower   = str_trim(tolower(EFO_Term)),
+    EFO_Parent_Term_lower = str_trim(tolower(EFO_Parent_Term)),
+    EFO_Parent_ID_lower = str_trim(tolower(EFO_Parent_ID)),
+    PHENOTYPE_lower  = str_trim(tolower(PHENOTYPE))
+  ) %>%
+  select(-EFO_ID, -EFO_Term, -EFO_Parent_Term, -EFO_Parent_ID)
+
+exact_id <- combined_data_epitope_coloc %>%
+  inner_join(
+    drug_summary %>%
+      select(
+        HARMONIZED_GENE_NAME, EFO_ID_lower,
+        drug_name, action_type, Indication_disease_name,
+        drug_max_phase, max_phase_for_ind, year_of_first_approval
+      ),
+    by = c("HARMONIZED_GENE_NAME", "EFO_ID_lower")
+  ) %>%
+  left_join(gene_drug_info, by = "HARMONIZED_GENE_NAME") %>%
+  mutate(
+    drug_rep = "Drug exists (Exact EFO_ID)",
+    match_type = "exact_efo_id",
+    drug_name = all_drug_names,
+    Indication_disease_name = all_indications
+  ) %>%
+  select(-all_drug_names, -all_indications) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+
+exact_term <- combined_data_epitope_coloc %>%
+  anti_join(exact_id %>% select(row_id), by = "row_id") %>%
+  inner_join(
+    drug_summary %>%
+      select(
+        HARMONIZED_GENE_NAME, EFO_Term_lower,
+        drug_name, action_type, Indication_disease_name,
+        drug_max_phase, max_phase_for_ind, year_of_first_approval
+      ),
+    by = c("HARMONIZED_GENE_NAME", "EFO_Term_lower")
+  ) %>%
+  left_join(gene_drug_info, by = "HARMONIZED_GENE_NAME") %>%
+  mutate(
+    drug_rep = "Drug exists (Exact EFO_Term)",
+    match_type = "exact_efo_term",
+    drug_name = all_drug_names,
+    Indication_disease_name = all_indications
+  ) %>%
+  select(-all_drug_names, -all_indications) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+
+parent_match <- combined_data_epitope_coloc %>%
+  anti_join(bind_rows(exact_id, exact_term) %>% select(row_id), by = "row_id") %>%
+  inner_join(
+    drug_summary %>%
+      select(
+        HARMONIZED_GENE_NAME, EFO_Parent_ID_lower,
+        drug_name, action_type, Indication_disease_name,
+        drug_max_phase, max_phase_for_ind, year_of_first_approval
+      ),
+    by = c("HARMONIZED_GENE_NAME", "EFO_Parent_ID_lower")
+  ) %>%
+  left_join(gene_drug_info, by = "HARMONIZED_GENE_NAME") %>%
+  mutate(
+    drug_rep = "Drug exists (Parent EFO)",
+    match_type = "parent_efo",
+    drug_name = all_drug_names,
+    Indication_disease_name = all_indications
+  ) %>%
+  select(-all_drug_names, -all_indications) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+phecode_match <- combined_data_epitope_coloc %>%
+  anti_join(bind_rows(exact_id, exact_term, parent_match) %>% select(row_id), by = "row_id") %>%
+  inner_join(
+    drug_summary %>%
+      select(
+        HARMONIZED_GENE_NAME, Genetic_phenotype_phecode_lower,
+        drug_name, action_type, Indication_disease_name,
+        drug_max_phase, max_phase_for_ind, year_of_first_approval
+      ),
+    by = c("HARMONIZED_GENE_NAME", "PHENOTYPE_lower" = "Genetic_phenotype_phecode_lower")
+  ) %>%
+  left_join(gene_drug_info, by = "HARMONIZED_GENE_NAME") %>%
+  mutate(
+    drug_rep = "Drug exists (Genetic_phenotype_phecode)",
+    match_type = "phecode",
+    drug_name = all_drug_names,
+    Indication_disease_name = all_indications
+  ) %>%
+  select(-all_drug_names, -all_indications) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+matched_pheno <- combined_data_epitope_coloc %>%
+  anti_join(bind_rows(exact_id, exact_term, parent_match, phecode_match) %>% select(row_id), by = "row_id") %>%
+  inner_join(
+    drug_summary %>%
+      select(
+        HARMONIZED_GENE_NAME, Matched_genetic_phenotype_lower,
+        drug_name, action_type, Indication_disease_name,
+        drug_max_phase, max_phase_for_ind, year_of_first_approval
+      ),
+    by = "HARMONIZED_GENE_NAME"
+  ) %>%
+  filter(PHENOTYPE_lower == Matched_genetic_phenotype_lower) %>%
+  left_join(gene_drug_info, by = "HARMONIZED_GENE_NAME") %>%
+  mutate(
+    drug_rep = "Drug exists (Matched_genetic_phenotype)",
+    match_type = "matched_pheno",
+    drug_name = all_drug_names,
+    Indication_disease_name = all_indications
+  ) %>%
+  select(-all_drug_names, -all_indications) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+
+has_drug_gene <- combined_data_epitope_coloc %>%
+  anti_join(bind_rows(exact_id, exact_term, parent_match, phecode_match, matched_pheno) %>% select(row_id), by = "row_id") %>%
+  semi_join(drug_summary %>% select(HARMONIZED_GENE_NAME) %>% distinct(), by = "HARMONIZED_GENE_NAME") %>%
+  left_join(
+    drug_summary %>%
+      group_by(HARMONIZED_GENE_NAME) %>%
+      summarise(
+        drug_name = paste(unique(drug_name), collapse = " | "),
+        action_type = paste(unique(action_type), collapse = " | "),
+        Indication_disease_name = paste(unique(Indication_disease_name), collapse = " | "),
+        drug_max_phase = paste(unique(drug_max_phase), collapse = " | "),
+        max_phase_for_ind = paste(unique(max_phase_for_ind), collapse = " | "),
+        year_of_first_approval = paste(unique(year_of_first_approval), collapse = " | "),
+        .groups = "drop"
+      ),
+    by = "HARMONIZED_GENE_NAME"
+  ) %>%
+  mutate(
+    drug_rep = "Drug repurposing",
+    match_type = "none"
+  ) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+dfs <- list(exact_id, exact_term, parent_match, phecode_match, matched_pheno, has_drug_gene)
+
+# Trova le colonne comuni
+common_cols <- Reduce(intersect, lapply(dfs, names))
+
+# Escludi row_id dalla conversione
+cols_to_convert <- setdiff(common_cols, "row_id")
+
+dfs_norm <- lapply(dfs, function(df) {
+  df %>% mutate(across(all_of(cols_to_convert), as.character))
+})
+
+no_drug_gene <- combined_data_epitope_coloc %>%
+  anti_join(
+    bind_rows(dfs_norm) %>% select(row_id),
+    by = "row_id"
+  ) %>%
+  mutate(
+    drug_rep = "No drug-gene match",
+    match_type = "no_drug_gene"
+  ) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+
+classified <- bind_rows(
+  exact_id,
+  exact_term,
+  parent_match,
+  phecode_match,
+  matched_pheno,
+  has_drug_gene,
+  no_drug_gene
+) %>%
+  select(any_of(c(
+    colnames(combined_data_epitope_coloc),
+    "drug_rep", "drug_name", "action_type",
+    "drug_indication", "match_type",
+    "Indication_disease_name",
+    "drug_max_phase", "max_phase_for_ind", "year_of_first_approval"
+  ))) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+no_drug_gene_norm <- no_drug_gene %>% mutate(across(any_of(cols_to_convert), as.character))
+classified <- bind_rows(
+  dfs_norm,
+  no_drug_gene_norm
+) %>%
+  select(any_of(c(
+    colnames(combined_data_epitope_coloc),
+    "drug_rep", "drug_name", "action_type",
+    "drug_indication", "match_type",
+    "Indication_disease_name",
+    "drug_max_phase", "max_phase_for_ind", "year_of_first_approval"
+  ))) %>%
+  distinct(row_id, .keep_all = TRUE)
+
+
+library(dplyr)
+
+# Create a lookup table collapsing duplicate gene matches
+drug_lookup <- drug_summary %>%
+  group_by(HARMONIZED_GENE_NAME) %>%
+  summarise(
+    Indication_disease_name = paste(unique(Indication_disease_name), collapse = " | "),
+    .groups = "drop"
+  )
+
+
+# --- Step: Add approved_indications_all per gene–drug ---
+
+# Create lookup with all approved indications per gene–drug
+approved_lookup <- drug_summary %>%
+  group_by(HARMONIZED_GENE_NAME, drug_name) %>%
+  summarise(
+    approved_indications_all = paste(unique(Indication_disease_name), collapse = " | "),
+    .groups = "drop"
+  )
+
+# Join the lookup to add approved_indications_all
+classified <- classified %>%
+  left_join(approved_lookup, by = c("HARMONIZED_GENE_NAME", "drug_name"))
+
+# For repurposing rows, clear Indication_disease_name (set to NA)
+classified_a <- classified %>%
+  mutate(
+    Indication_disease_name = if_else(
+      drug_rep == "Drug repurposing",
+      NA_character_,
+      Indication_disease_name
+    )
+  )
+
+table2 <- read_xlsx("/home/giulia.pontali/supplementary_table_2 (7).xlsx", sheet=2)
+
+final2 <- merge(
+  classified,
+  table2,
+  by.x = c("seqID", "HARMONIZED_GENE_NAME"),
+  by.y = c("SeqID", "HGNC_Symbol"),
+  all.x = TRUE,
+  sort = FALSE
+)
+
+final2 <- final2 %>%
+  group_by(SNP) %>%
+  mutate(n_other_phenotypes_for_SNP = n() - 1) %>%
+  ungroup()
+
+
+write_xlsx(final2, "/home/giulia.pontali/final_drug.xlsx")
+
+
+
